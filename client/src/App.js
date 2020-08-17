2fec16a3ea12c6d488018c40f90c7a227a146eb5
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-08-17 16:01:19 -0400

    got data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, Route, Switch, Router } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
 
-const App = () => {
+const App = (props) => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
@@ -20,15 +21,14 @@ const App = () => {
     }
     getMovies();
   }, []);
-
+  
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+     
     </div>
   );
 };
